ee637f7a53fee52c9904bc5ae9a65424178a1e30
Javier <za210110217@zapopan.tecmm.edu.mx>
2023-02-15 16:52:09 -0600

    se agregaron nuevos temas

--- Fundamentosdepython.py
+++ Fundamentosdepython.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     print("Operador de division entera (10 // 3): ", 10//3)
     print("Operador de potencias (5 ** 5):", 5**5)
     print("Operador de cambio de signo (-5): ",-5)
-    
+    #Actividad: Mostra las tablas de verdad de los operadores logicos and not or    
     print("====Operadores logicos====")
     print("Operador and (True and False):", True and False)
     print("Operador and (False and True):", True and True)
@@ -48,9 +48,66 @@ if __name__ == "__main__":
     a,b,c=98,3.1416,"Bienvenido"
     print(a,b,c)
 
-    
+    nuevoTema("Enteros")
+    w=105
+    x=233243423423
+    y=-21332131
+    z=0b00101010101
+    h= 0xffa #Entero en hecadecimal
 
-       
-    
-    #Actividad: Mostra las tablas de verdad de los operadores logicos and not or
-    
+    print(w,type(w))
+    print(x,type(x))
+    print(y,type(y))
+    print(z,type(z))
+    print(h,type(h))
+
+
+    nuevoTema("Flotantes")
+    x=1297.5
+    print(x, type(x))
+    y=0.32432423
+    print(y, type(y))
+
+    nuevoTema("Numero Complejos")
+    x=-46j
+    y=32+45j
+    z=2j
+
+    print(x,type(x))
+    print(y,type(y))
+    print(z,type(z))
+
+    nuevoTema("Listas")
+    a=[10,20.5,"PythonList"]
+    print(a)
+    a=["lista2",45,16.3j]
+    print(a)
+    print(a,[2])
+
+    nuevoTema("Tuplas") #Las Tuplas son estables
+    t=(25,"Tupla",5.6)
+    print(t)
+    print(t[1])
+    #t[0]="Modificado" Las tuplas no permiten que se rescriban los datos
+    #print(t)
+
+    nuevoTema("Los conjuntos")
+    c = {50, 20, 10, 4, 8, 50} #Los conjuntos usan las llaves
+    print(c)
+
+    #json con una clave se almacena un nombre y un valor
+
+    d={1:"Valor2","2":45}
+    print(d)
+    print(d["2"])
+    print(d[1])
+
+    nuevoTema("Cadena") #segun las comillas es el texto que puedes escribir, y el tabulador recorre el texto
+    cadena1="cadena entre comillas dobles"
+    print(cadena1)
+    cadena2='cadena entre comillas sencillas'
+    print(cadena2)
+    cadena3='''cadena de 
+    varias
+    lineas'''
+    print(cadena3)
